5bf816216666405a6e3590c8133b4e7a8b112505
204184251HQG <204184251@qq.com>
2021-08-11 20:22:25 +0800

    some counter code

---
+++ keilib/include/util.h
@@ -0,0 +1,17 @@
+#ifndef _KEIUTIL_H_
+#define _KEIUTIL_H_
+
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#define ACCESS_ONCE(x) (*(volatile typeof(x) *)&(x))
+#define WRITE_ONCE(dst, src) (ACCESS_ONCE(dst) = (typeof(dst))(src))
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- keilib/src/parallel_tools/counter.c
+++ keilib/src/parallel_tools/counter.c
@@ -1,10 +1,19 @@
+#include "util.h"
 #include "log/keilog.h"
 #include "parallel_tools/counter.h"
 
 typedef uint32_t counter_uint;
 
 __thread counter_uint counter = 0;
+__thread counter_uint countermax = 0;
 
 int add_count(counter_uint delta)
-{}
+{
+    if(countermax - counter >= delta){
+        WRITE_ONCE(counter, counter + delta);
+        return 1;
+    }
+    
+
+}
 
