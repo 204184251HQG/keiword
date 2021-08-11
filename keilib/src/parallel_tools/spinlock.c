5e1c6331646d3824249fa2248b680d3db0cfb829
204184251HQG <204184251@qq.com>
2021-08-11 20:26:45 +0800

    simple spinlock

---
+++ keilib/src/parallel_tools/spinlock.c
@@ -0,0 +1,37 @@
+#include "util.h"
+
+
+
+static int section_lock16(uint16_t *lock) {
+    int waitc = 0;
+    do {
+        if (*lock == 0) {
+            if (InterlockedCompareExchange16((SHORT volatile*)lock, 1, 0) == 0) {
+                log_debug("lock16ed");
+                return 0;
+            }
+        }
+        log_info("lock16 waitting %d", waitc);
+        
+        do {
+            __asm {pause};
+            if (*lock == 0) {
+                break;
+            }
+            if (waitc++ > 30) {
+                waitc = 0;
+                SwitchToThread();
+            }
+        } while (1);
+        
+    } while (1);
+}
+
+static int section_unlock16(uint16_t *lock) {
+    if (InterlockedCompareExchange16((SHORT volatile*)lock, 0, 1) == 0) {
+        log_info("unlock16 error, no in lock");
+        return -1;
+    }
+    log_debug("unlock16ed");
+    return 0;
+}
